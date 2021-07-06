cbbca0cbb7646195dd700b7f1c4ebe8d2d465f39
p_shesterikov <p_shesterikov@wargaming.net>
2021-07-06 13:17:07 +0300

    validate link set with different promo

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,20 @@
+import pytest
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/?promo=newYear2019"
+def get_links_to_valid():
+    links_to_validate = [
+        pytest.param(
+            f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer_num}',
+            marks=pytest.mark.xfail(reason='will not fix') if offer_num == 7 else ()
+        )
+        for offer_num in range(0, 10)
+    ]
+    return links_to_validate
+
+
+@pytest.mark.parametrize('link', get_links_to_valid())
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     item_to_add_name = page.get_item_name()
